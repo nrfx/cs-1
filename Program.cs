1323621c7c1b7d44e16b78f5206d37467ac48bf7
nrfx <zavovaz001@gmail.com>
2026-02-23 16:14:12 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("введите три числа через пробел");
+string inpt = Console.ReadLine();
+
+int numb_inpt = Convert.ToInt32(inpt);
