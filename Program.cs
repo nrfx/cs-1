273cf15448b5315d9f94fc31de23c8e2169acfb6
nrfx <zavovaz001@gmail.com>
2026-02-23 16:41:23 +0800

    все работает

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿
 Console.Write("введите три числа по очереди \n");
 
-string a = Console.ReadLine();
-string b = Console.ReadLine();
-string c = Console.ReadLine();
+string a_str = Console.ReadLine();
+string b_str = Console.ReadLine();
+string c_str = Console.ReadLine();
 
-int a_int = Convert.ToInt32(a);
-int b_int = Convert.ToInt32(b);
-int c_int = Convert.ToInt32(c);
+int a = Convert.ToInt32(a_str);
+int b = Convert.ToInt32(b_str);
+int c = Convert.ToInt32(c_str);
 
-Console.WriteLine($"Числа: {a_int} {b_int} {c_int}");
+Console.WriteLine($"Числа: {a} {b} {c}");
+
+if (a > b && a > c)
+{
+    int res = b * c;
+    Console.WriteLine($"результат: {res}");
+}
+else if (b > a && b > c)
+{
+    int res = a * c;
+    Console.WriteLine(res);
+}
+else
+{
+    int res = a * b;
+    Console.WriteLine(res);
+}
