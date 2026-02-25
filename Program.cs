daacef2ddc329f2b273b42ef2e9567205687d275
nrfx <zavovaz001@gmail.com>
2026-02-25 12:27:46 +0800

    преобразовал код вида program main и сделал отдельный класс логики как по гайду

--- Program.cs
+++ Program.cs
@@ -1,29 +1,48 @@
-﻿
-Console.Write("введите три числа по очереди \n");
+﻿using System;
 
-string a_str = Console.ReadLine();
-string b_str = Console.ReadLine();
-string c_str = Console.ReadLine();
-
-int a = Convert.ToInt32(a_str);
-int b = Convert.ToInt32(b_str);
-int c = Convert.ToInt32(c_str);
-
-
-int res = 0;
-
-if (a > b && a > c)
+namespace MyConsoleApp
 {
-    res = b * c;
-}
-else if (b > a && b > c)
-{
-    res = a * c;
-}
-else
-{
-    res = a * b;
-}
+    public class Logic
+    {
+        public static int Calculate(int a, int b, int c)
+        {
+            int res = 0;
+            if (a > b && a > c)
+            {
+                res = b * c;
+                return res;
+            }
+            else if (b > a && b > c)
+            {
+                res = a * c;
+                return res;
+            }
+            else
+            {
+                res = a * b;
+                return res;
+            }
+        }
 
-Console.WriteLine($"Числа: {a} {b} {c}");
-Console.WriteLine($"результат: {res}");
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите три числа по очереди \n");
+
+            string a_str = Console.ReadLine();
+            string b_str = Console.ReadLine();
+            string c_str = Console.ReadLine();
+
+            int a = Convert.ToInt32(a_str);
+            int b = Convert.ToInt32(b_str);
+            int c = Convert.ToInt32(c_str);
+
+            int res = Logic.Calculate(a, b, c);
+
+            Console.WriteLine($"Числа: {a} {b} {c}");
+            Console.WriteLine($"результат: {res}");
+        }
+    }
+}
